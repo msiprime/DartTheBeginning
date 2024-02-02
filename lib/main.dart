a2a49635b09f93ab176a93c9b3bb2dcd46ed46a3
msiprime <msisakib958@gmail.com>
2024-02-02 22:08:21 +0600

    image with loading from online

--- lib/main.dart
+++ lib/main.dart
@@ -1,93 +1,33 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+  runApp(
+    MaterialApp(
+        home: Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text('I Am Rich', style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.blueGrey[900],
+        centerTitle: true,
+        // titleTextStyle: TextStyle(color: Colors.white),
       ),
-      body: Container(
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              //crossAxisAlignment: ,
-              children: <Widget>[
-                Text('01'),
-                Text('02'),
-                Text('03'),
-                Text('04'),
-                Text('05'),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: Text('Button Text'),
-                )
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              //crossAxisAlignment: ,
-              children: <Widget>[
-                Text('06'),
-                Text('07'),
-                Text('08'),
-                Text('09'),
-                Text('10'),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: Text('Button Text'),
-                )
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              //crossAxisAlignment: ,
-              children: <Widget>[
-                Text('11'),
-                Text('12'),
-                Text('13'),
-                Text('14'),
-                Text('15'),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: Text('Button Text'),
-                )
-              ],
-            )
-          ],
+      backgroundColor: Colors.blueGrey,
+      body: Center(
+        child: Image.network(
+          'https://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/nebula_blue.s2014.png',
+          // You can update this with your desired image URL
+          loadingBuilder: (context, child, loadingProgress) {
+            if (loadingProgress == null) return child;
+            return CircularProgressIndicator(
+              value: loadingProgress.expectedTotalBytes != null
+                  ? loadingProgress.cumulativeBytesLoaded /
+                      loadingProgress.expectedTotalBytes!
+                  : null,
+            );
+          },
+          errorBuilder: (BuildContext context, url, error) =>
+              const Text('Error loading image'),
         ),
       ),
-    );
-  }
+    )),
+  );
 }
