3fdb0f41fe905bcd4ec577958a01fff865eaff28
msiprime <msisakib958@gmail.com>
2024-02-04 17:28:51 +0600

    Dice App is finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,61 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: const DicePage(),
+        backgroundColor: Colors.red[800],
+        appBar: AppBar(
+          title: const Text('Dicey', style: TextStyle(color: Colors.white)),
+          backgroundColor: Colors.red[800],
+          centerTitle: true,
+        ),
+      ),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceRoll = 1;
+  int rightDiceRoll = 1;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal[600],
-        body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                color: Colors.red,
-                width: 100,
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    height: 100,
-                    width: 100,
-                    color: Colors.yellow,
-                  ),
-                  Container(
-                    height: 100,
-                    width: 100,
-                    color: Colors.green,
-                  )
-                ],
-              ),
-              Container(
-                color: Colors.blue,
-                width: 100,
-              )
-            ],
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+                onPressed: () {
+                  changeDiceFace();
+                },
+                child: Image.asset('images/dice$leftDiceRoll.png')),
           ),
-        ),
+          Expanded(
+            child: TextButton(
+                onPressed: () {
+                  changeDiceFace();
+                },
+                child: Image.asset('images/dice$rightDiceRoll.png')),
+          ),
+        ],
       ),
     );
+  }
+
+  void changeDiceFace() {
+    setState(() {
+      leftDiceRoll = Random().nextInt(6) + 1;
+      rightDiceRoll = Random().nextInt(6) + 1;
+    });
   }
 }
--- lib/practice.dart
+++ lib/practice.dart
@@ -18,3 +18,54 @@ void slowDrive() {
 void fastDrive() {
   print('Driving Blazing Fast');
 }
+/*
+* import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal[600],
+        body: SafeArea(
+          child: Column(
+            children: [
+              CircleAvatar(
+                radius: 40,
+                backgroundImage: AssetImage('images/diamond.png'),
+                backgroundColor: Colors.teal[600],
+              ),
+              Text(
+                'Tanjila Ayrin',
+                style: TextStyle(
+                    fontFamily: 'Pacifico', fontSize: 30, color: Colors.white),
+              ),
+              Text(
+                'DOES NOTHING',
+                style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    letterSpacing: 2.5,
+                    fontSize: 20,
+                    color: Colors.teal[50]),
+              ),
+              Container(
+                child: Row(
+                  children: [
+                    Icon(Icons.add_shopping_cart),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+*/
